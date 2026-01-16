3a1d28eceea0c59b3eeb5158984db7b9765411ff
MMLAB-05 <MMLAB-05@MMLAB-05>
2026-01-16 18:01:15 +0800

    qwe

--- LibraryCatalog/Form1.Designer.cs
+++ LibraryCatalog/Form1.Designer.cs
@@ -57,6 +57,7 @@
             lblTotalBooks = new ToolStripLabel();
             lblStatus = new ToolStripLabel();
             lblError = new ToolStripLabel();
+            btnExport = new Button();
             ((System.ComponentModel.ISupportInitialize)dgBooks).BeginInit();
             gbActionManagement.SuspendLayout();
             gbFilteringSearching.SuspendLayout();
@@ -65,84 +66,85 @@
             // 
             // txtISBN
             // 
-            txtISBN.Font = new Font("Arial Narrow", 12F);
+            txtISBN.Font = new Font("Arial Rounded MT Bold", 9F);
             txtISBN.Location = new Point(86, 57);
             txtISBN.Name = "txtISBN";
-            txtISBN.Size = new Size(257, 26);
+            txtISBN.Size = new Size(257, 21);
             txtISBN.TabIndex = 0;
             // 
             // txtTitle
             // 
-            txtTitle.Font = new Font("Arial Narrow", 12F);
+            txtTitle.Font = new Font("Arial Rounded MT Bold", 9F);
             txtTitle.Location = new Point(86, 92);
             txtTitle.Name = "txtTitle";
-            txtTitle.Size = new Size(257, 26);
+            txtTitle.Size = new Size(257, 21);
             txtTitle.TabIndex = 1;
             // 
             // txtAuthor
             // 
-            txtAuthor.Font = new Font("Arial Narrow", 12F);
+            txtAuthor.Font = new Font("Arial Rounded MT Bold", 9F);
             txtAuthor.Location = new Point(86, 129);
             txtAuthor.Name = "txtAuthor";
-            txtAuthor.Size = new Size(257, 26);
+            txtAuthor.Size = new Size(257, 21);
             txtAuthor.TabIndex = 2;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Arial Narrow", 12F);
+            label1.Font = new Font("Georgia", 9.75F);
             label1.Location = new Point(22, 60);
             label1.Name = "label1";
-            label1.Size = new Size(39, 20);
+            label1.Size = new Size(38, 16);
             label1.TabIndex = 3;
             label1.Text = "ISBN";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Arial Narrow", 12F);
+            label2.Font = new Font("Georgia", 9.75F);
             label2.Location = new Point(22, 92);
             label2.Name = "label2";
-            label2.Size = new Size(33, 20);
+            label2.Size = new Size(35, 16);
             label2.TabIndex = 4;
             label2.Text = "Title";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Arial Narrow", 12F);
+            label3.Font = new Font("Georgia", 9.75F);
             label3.Location = new Point(22, 129);
             label3.Name = "label3";
-            label3.Size = new Size(47, 20);
+            label3.Size = new Size(52, 16);
             label3.TabIndex = 5;
             label3.Text = "Author";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Cooper Black", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label4.Location = new Point(29, 9);
+            label4.Font = new Font("Georgia", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(48, 14);
             label4.Name = "label4";
-            label4.Size = new Size(200, 19);
+            label4.Size = new Size(239, 23);
             label4.TabIndex = 6;
             label4.Text = "Library Catalog System";
             // 
             // cmbCategory
             // 
+            cmbCategory.Font = new Font("Arial Rounded MT Bold", 9F);
             cmbCategory.FormattingEnabled = true;
             cmbCategory.Items.AddRange(new object[] { "Fiction", "Non-Fiction", "Science", "History", "Misc" });
             cmbCategory.Location = new Point(86, 166);
             cmbCategory.Name = "cmbCategory";
-            cmbCategory.Size = new Size(257, 23);
+            cmbCategory.Size = new Size(257, 22);
             cmbCategory.TabIndex = 7;
             // 
             // chkAvailable
             // 
             chkAvailable.AutoSize = true;
-            chkAvailable.Font = new Font("Arial Narrow", 12F);
+            chkAvailable.Font = new Font("Georgia", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             chkAvailable.Location = new Point(84, 198);
             chkAvailable.Name = "chkAvailable";
-            chkAvailable.Size = new Size(97, 24);
+            chkAvailable.Size = new Size(100, 20);
             chkAvailable.TabIndex = 8;
             chkAvailable.Text = "Is Available";
             chkAvailable.UseVisualStyleBackColor = true;
@@ -150,16 +152,16 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Arial Narrow", 12F);
+            label5.Font = new Font("Georgia", 9.75F);
             label5.Location = new Point(17, 165);
             label5.Name = "label5";
-            label5.Size = new Size(63, 20);
+            label5.Size = new Size(63, 16);
             label5.TabIndex = 9;
             label5.Text = "Category";
             // 
             // btnAddBook
             // 
-            btnAddBook.Font = new Font("Arial Narrow", 12F);
+            btnAddBook.Font = new Font("Georgia", 9F);
             btnAddBook.Location = new Point(74, 235);
             btnAddBook.Name = "btnAddBook";
             btnAddBook.Size = new Size(90, 27);
@@ -170,17 +172,18 @@
             // 
             // btnUpdateBook
             // 
-            btnUpdateBook.Font = new Font("Arial Narrow", 12F);
+            btnUpdateBook.Font = new Font("Georgia", 9F);
             btnUpdateBook.Location = new Point(170, 235);
             btnUpdateBook.Name = "btnUpdateBook";
             btnUpdateBook.Size = new Size(91, 27);
             btnUpdateBook.TabIndex = 11;
             btnUpdateBook.Text = "Update Book";
             btnUpdateBook.UseVisualStyleBackColor = true;
+            btnUpdateBook.Click += btnUpdateBook_Click;
             // 
             // btnClearInputs
             // 
-            btnClearInputs.Font = new Font("Arial Narrow", 12F);
+            btnClearInputs.Font = new Font("Georgia", 9F);
             btnClearInputs.Location = new Point(267, 235);
             btnClearInputs.Name = "btnClearInputs";
             btnClearInputs.Size = new Size(88, 27);
@@ -195,12 +198,14 @@
             dgBooks.Name = "dgBooks";
             dgBooks.Size = new Size(643, 502);
             dgBooks.TabIndex = 13;
+            dgBooks.CellClick += dgBooks_CellClick;
             // 
             // gbActionManagement
             // 
             gbActionManagement.Controls.Add(chkConfirmDelete);
             gbActionManagement.Controls.Add(btnClearAll);
             gbActionManagement.Controls.Add(btnRemoveSelected);
+            gbActionManagement.Font = new Font("Georgia", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             gbActionManagement.Location = new Point(29, 308);
             gbActionManagement.Name = "gbActionManagement";
             gbActionManagement.Size = new Size(283, 137);
@@ -211,33 +216,34 @@
             // chkConfirmDelete
             // 
             chkConfirmDelete.AutoSize = true;
-            chkConfirmDelete.Font = new Font("Arial Narrow", 11.25F);
+            chkConfirmDelete.Font = new Font("Georgia", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
             chkConfirmDelete.Location = new Point(80, 98);
             chkConfirmDelete.Name = "chkConfirmDelete";
-            chkConfirmDelete.Size = new Size(118, 24);
+            chkConfirmDelete.Size = new Size(135, 22);
             chkConfirmDelete.TabIndex = 2;
             chkConfirmDelete.Text = "Confirm Delete?";
             chkConfirmDelete.UseVisualStyleBackColor = true;
             // 
             // btnClearAll
             // 
-            btnClearAll.Font = new Font("Arial Narrow", 11.25F);
-            btnClearAll.Location = new Point(153, 32);
+            btnClearAll.Font = new Font("Georgia", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnClearAll.Location = new Point(141, 35);
             btnClearAll.Name = "btnClearAll";
-            btnClearAll.Size = new Size(103, 55);
+            btnClearAll.Size = new Size(126, 48);
             btnClearAll.TabIndex = 1;
             btnClearAll.Text = "Clear All";
             btnClearAll.UseVisualStyleBackColor = true;
             // 
             // btnRemoveSelected
             // 
-            btnRemoveSelected.Font = new Font("Arial Narrow", 11.25F);
-            btnRemoveSelected.Location = new Point(19, 32);
+            btnRemoveSelected.Font = new Font("Georgia", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnRemoveSelected.Location = new Point(19, 35);
             btnRemoveSelected.Name = "btnRemoveSelected";
-            btnRemoveSelected.Size = new Size(103, 55);
+            btnRemoveSelected.Size = new Size(116, 48);
             btnRemoveSelected.TabIndex = 0;
             btnRemoveSelected.Text = "Remove Selected";
             btnRemoveSelected.UseVisualStyleBackColor = true;
+            btnRemoveSelected.Click += btnRemoveSelected_Click;
             // 
             // gbFilteringSearching
             // 
@@ -247,6 +253,7 @@
             gbFilteringSearching.Controls.Add(label6);
             gbFilteringSearching.Controls.Add(txtSearch);
             gbFilteringSearching.Controls.Add(cmbFilterCategory);
+            gbFilteringSearching.Font = new Font("Georgia", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             gbFilteringSearching.Location = new Point(516, 8);
             gbFilteringSearching.Name = "gbFilteringSearching";
             gbFilteringSearching.Size = new Size(643, 100);
@@ -256,31 +263,33 @@
             // 
             // btnResetFilter
             // 
-            btnResetFilter.Font = new Font("Arial", 9F);
+            btnResetFilter.Font = new Font("Georgia", 9.75F);
             btnResetFilter.Location = new Point(532, 48);
             btnResetFilter.Name = "btnResetFilter";
             btnResetFilter.Size = new Size(105, 23);
             btnResetFilter.TabIndex = 5;
             btnResetFilter.Text = "Reset Filter";
             btnResetFilter.UseVisualStyleBackColor = true;
+            btnResetFilter.Click += btnResetFilter_Click;
             // 
             // btnApplyFilter
             // 
-            btnApplyFilter.Font = new Font("Arial", 9F);
+            btnApplyFilter.Font = new Font("Georgia", 9.75F);
             btnApplyFilter.Location = new Point(425, 48);
             btnApplyFilter.Name = "btnApplyFilter";
             btnApplyFilter.Size = new Size(90, 23);
             btnApplyFilter.TabIndex = 4;
             btnApplyFilter.Text = "Apply Filter";
             btnApplyFilter.UseVisualStyleBackColor = true;
+            btnApplyFilter.Click += btnApplyFilter_Click;
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Font = new Font("Arial", 9F);
+            label7.Font = new Font("Arial", 9F, FontStyle.Bold);
             label7.Location = new Point(173, 24);
             label7.Name = "label7";
-            label7.Size = new Size(46, 15);
+            label7.Size = new Size(48, 15);
             label7.TabIndex = 3;
             label7.Text = "Search";
             label7.Click += label7_Click;
@@ -288,10 +297,10 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Font = new Font("Arial", 9F);
+            label6.Font = new Font("Arial", 9F, FontStyle.Bold);
             label6.Location = new Point(9, 24);
             label6.Name = "label6";
-            label6.Size = new Size(56, 15);
+            label6.Size = new Size(58, 15);
             label6.TabIndex = 2;
             label6.Text = "Category";
             // 
@@ -307,6 +316,7 @@
             // 
             cmbFilterCategory.Font = new Font("Arial", 9F);
             cmbFilterCategory.FormattingEnabled = true;
+            cmbFilterCategory.Items.AddRange(new object[] { "Fiction", "Non-Fiction", "Science", "History", "Misc" });
             cmbFilterCategory.Location = new Point(6, 49);
             cmbFilterCategory.Name = "cmbFilterCategory";
             cmbFilterCategory.Size = new Size(149, 23);
@@ -324,19 +334,32 @@
             // 
             // lblTotalBooks
             // 
+            lblTotalBooks.Font = new Font("Georgia", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             lblTotalBooks.Name = "lblTotalBooks";
             lblTotalBooks.Size = new Size(0, 22);
             // 
             // lblStatus
             // 
+            lblStatus.Font = new Font("Georgia", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             lblStatus.Name = "lblStatus";
             lblStatus.Size = new Size(0, 22);
             // 
             // lblError
             // 
+            lblError.Font = new Font("Georgia", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
             lblError.ForeColor = Color.Red;
             lblError.Name = "lblError";
             lblError.Size = new Size(0, 22);
+            // 
+            // btnExport
+            // 
+            btnExport.Location = new Point(119, 462);
+            btnExport.Name = "btnExport";
+            btnExport.Size = new Size(94, 34);
+            btnExport.TabIndex = 17;
+            btnExport.Text = "Export";
+            btnExport.UseVisualStyleBackColor = true;
+            btnExport.Click += btnExport_Click;
             // 
             // Form1
             // 
@@ -344,6 +367,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(1171, 628);
+            Controls.Add(btnExport);
             Controls.Add(toolStrip1);
             Controls.Add(gbFilteringSearching);
             Controls.Add(gbActionManagement);
@@ -408,5 +432,6 @@
         private ToolStripLabel lblTotalBooks;
         private ToolStripLabel lblStatus;
         private ToolStripLabel lblError;
+        private Button btnExport;
     }
 }
--- LibraryCatalog/Form1.cs
+++ LibraryCatalog/Form1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace LibraryCatalog
 {
     public partial class Form1 : Form
@@ -15,9 +17,10 @@ namespace LibraryCatalog
                 IsAvailable = true
             });
 
+            DisplayTotalBooks();
+            DisplayStatus("Ready");
             dgBooks.DataSource = _catalog;
-            lblTotalBooks.Text = string.Format("Total Books: {0}", _catalog.Count.ToString());
-            lblStatus.Text = "Ready";
+
         }
 
         private void label7_Click(object sender, EventArgs e)
@@ -68,13 +71,175 @@ namespace LibraryCatalog
 
             dgBooks.DataSource = null;
             dgBooks.DataSource = _catalog;
-            lblTotalBooks.Text = string.Format("Total Books: {0}", _catalog.Count.ToString());
-            lblStatus.Text = "Book Added!";
 
+            RefreshDataGrid();
+            DisplayStatus("Book Added!");
+            ClearContents();
+        }
+
+        private void dgBooks_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var rowIndex = e.RowIndex;
+            var isbn = dgBooks.Rows[e.RowIndex].Cells[0].Value.ToString();
+
+            Book book = _catalog.First(m => m.ISBN == isbn);
+
+            txtISBN.Text = book.ISBN;
+            txtTitle.Text = book.Title;
+            txtAuthor.Text = book.Author;
+            cmbCategory.Text = book.Category;
+            chkAvailable.Checked = book.IsAvailable;
+
+            txtISBN.Enabled = false;
+        }
+
+        private void btnUpdateBook_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtISBN.Text))
+            {
+                lblError.Text = "Please Select Book First";
+                return;
+            }
+
+            Book book = _catalog.First(m => m.ISBN == txtISBN.Text);
+            book.Title = txtTitle.Text;
+            book.Author = txtAuthor.Text;
+            book.Category = cmbCategory.Text;
+            book.IsAvailable = chkAvailable.Checked;
+
+            RefreshDataGrid();
+            DisplayStatus("Book Update");
+            ClearContents();
+
+            txtISBN.Enabled = true;
+        }
+
+        private void ClearContents()
+        {
             txtISBN.Clear();
             txtTitle.Clear();
             txtAuthor.Clear();
             chkAvailable.Checked = false;
+            chkConfirmDelete.Checked = false;
+            lblError.Text = string.Empty;
+        }
+
+        private void DisplayStatus(string message)
+        {
+            lblStatus.Text = message;
+        }
+
+        private void RefreshDataGrid()
+        {
+            dgBooks.DataSource = null;
+            dgBooks.DataSource = _catalog;
+        }
+
+        private void DisplayTotalBooks()
+        {
+            lblTotalBooks.Text = string.Format("Total Books: {0}", _catalog.Count.ToString());
+        }
+
+        private void btnRemoveSelected_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtISBN.Text))
+            {
+                lblError.Text = "Please Select Book First";
+                return;
+            }
+
+            Book book = _catalog.First(m => m.ISBN == txtISBN.Text);
+
+            if (chkConfirmDelete.Checked)
+            {
+                DialogResult result = MessageBox.Show("Are you sure you want to delete this book?", "Confirm Delete",
+                    MessageBoxButtons.YesNo);
+
+                if (result == DialogResult.Yes)
+                {
+                    _catalog.Remove(book);
+                    DisplayStatus("Book Deleted!");
+                }
+            }
+
+            RefreshDataGrid();
+            DisplayTotalBooks();
+            ClearContents();
+
+            txtISBN.Enabled = true;
+        }
+
+        private void btnApplyFilter_Click(object sender, EventArgs e)
+        {
+            List<Book> filteredBooks = _catalog
+                    .Where(m =>
+                    m.Category == cmbFilterCategory.Text ||
+                    m.Title.Contains(txtSearch.Text)
+                    ).ToList();
+            dgBooks.DataSource = null;
+            dgBooks.DataSource = filteredBooks;
+        }
+
+        private void btnResetFilter_Click(object sender, EventArgs e)
+        {
+            RefreshDataGrid();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            ExportDataGridViewToCsv(dgBooks);
+        }
+
+        private void ExportDataGridViewToCsv(DataGridView dgv)
+        {
+            if (dgv.Rows.Count == 0)
+            {
+                MessageBox.Show("No data to export.");
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "CSV files (*.csv)|*.csv",
+                FileName = "LibraryCatalog.csv"
+            };
+
+            if (saveFileDialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            StringBuilder csv = new StringBuilder();
+
+            // Header row
+            for (int i = 0; i < dgv.Columns.Count; i++)
+            {
+                csv.Append(dgv.Columns[i].HeaderText);
+                if (i < dgv.Columns.Count - 1)
+                    csv.Append(",");
+            }
+            csv.AppendLine();
+
+            // Data rows
+            foreach (DataGridViewRow row in dgv.Rows)
+            {
+                if (row.IsNewRow) continue;
+
+                for (int i = 0; i < dgv.Columns.Count; i++)
+                {
+                    string value = row.Cells[i].Value?.ToString() ?? string.Empty;
+
+                    // Escape commas and quotes
+                    value = value.Replace("\"", "\"\"");
+                    csv.Append($"\"{value}\"");
+
+                    if (i < dgv.Columns.Count - 1)
+                        csv.Append(",");
+                }
+                csv.AppendLine();
+            }
+
+            File.WriteAllText(saveFileDialog.FileName, csv.ToString(), Encoding.UTF8);
+
+            MessageBox.Show("Export completed successfully!", "Export CSV");
         }
     }
 }
